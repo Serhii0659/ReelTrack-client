6b610c29b3535d2c8076ba7dd1920a7b7a38604a
Serhii Kovalenko <sergei.kovalenko.2006@gmail.com>
2025-05-12 23:44:23 +0300

    Update tailwind.config.js
    Co-Authored-By: kartuj4 <181428094+kartuj4@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Переконайтесь, що охоплює всі ваші файли з класами
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
